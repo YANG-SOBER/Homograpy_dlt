c577f15aeb42edbd5da99679fab4c5c425263ae3
YANG-SOBER <88379706+YANG-SOBER@users.noreply.github.com>
2021-09-13 11:42:05 +0800

    Update dlt.py

--- dlt.py
+++ dlt.py
@@ -1,83 +1,86 @@
-import cv2
-import numpy as np
-import re
-import math
-from numpy.linalg import inv, norm
-
-
 class Dlt(object):
-    """docstring for Dlt."""
-    def __init__(self, orgs,corrs):
-        self.orgs = orgs
-        self.corrs = corrs
 
-    def average(org, corrs):
-        averageOrg = np.zeros(2)
-        averageCorr = np.zeros(2)
-        for point in org:
-            averageOrg[0] += point[0]
-            averageOrg[1] += point[1]
-        for point in corres:
-            averageCorr[0] += point[0]
-            averageCorr[1] += point[1]
-        averageOrg /= len(org)
-        averageCorr /= len(corrs)
+  # constructor
+  def __init__(self, orgs, corrs):
+    self.orgs = orgs      # set of key points from first image set  [(), (), (), ...]
+    self.corrs = corrs    # set of key points from second image set [(), (), (), ...]
+  
+  # compute the centroid of a set of key points from an image
+  def average(self):
+    sumOrgs = np.zeros(2)
+    sumCorrs = np.zeros(2)
+    
+    for point in self.orgs:
+      sumOrgs[0] += point[0] # sum of the x-coordinates of the key points in the first image set
+      sumOrgs[1] += point[1] # sum of the y-coordinates of ...                   second ...
+    
+    for point in self.corrs:
+      sumCorrs[0] += point[0] # sum of the x-coordinates of the key points in the second image set
+      sumCorrs[1] += point[1] # sum of the y-coordinates of ...                   second ...
 
-        return averageOrg, averageCorr
+    averageOrgs = sumOrgs / len(self.orgs)
+    averageCorrs = sumCorrs / len(self.Corrs)  
+    
+    return averageOrgs, averageCorrs
+  
+  # compute the scale s, which will be used in T_norm, T_norm s in two image are different
+  def scale(self, points, average):
+    sumOfDist = 0.0
 
+    for point in points:
+      sumOfDist += np.linalg.norm(point - average)
+    
+    meanOfDist = sumOfDist / len(points) # mean distance of all key points from centroid (in one image)
+    s = math.sqrt(2) / meanOfDist # s is scale
 
-    def __scale(points, average):
-        sumOfDist = 0.0
-        for point in points:
-            sumOfDist += norm(point - average)
-        s = math.sqrt(2) * len(points) / (sumOfDist)
-        return s
+    return s
 
+  # compute T_norm
+  def matrixT(self, s, average):
+    Cx = average[0]
+    Cy = average[1]
+    T = np.float64([[s, 0, -s * Cx], 
+                  [0, s, -s * Cy], 
+                  [0, 0, 1]])
+    return T
+  
+  # normalize keypoints
+  def normalize(self, points, T):
+    normalizedPoints = np.zeros((len(points), 3))
+    for i, point in enumerate(points):
+      normalizedPoints[i] = T @ point
+    return normalizedPoints
+  
+  # Ah = 0 A: 2ix9, i is the number of point correspondences
+  def matrixA(self, normalizedOrgs, normalizedCorrs):
+    A = np.zeros((len(normalizedOrgs)*2, 9))
+    for i in A.shape[0]:
+      A[i]= np.float64([0, 0, 0, -normalizedOrgs[i][0], -normalizedOrgs[i][1], -1, 
+                        normalizedCorrs[i][1] * normalizedOrgs[i][0], normalizedCorrs[i][1] * normalizedOrgs[i][1], normalizedCorrs[i][1]])
+                      
+      A[i+1] = np.float64([normalizedOrgs[i][0], normalizedOrgs[i][1], 1, 0, 0, 0, 
+                           -normalizedCorrs[i][0] * normalizedOrgs[i][0], -normalizedCorrs[i][0]* normalizedOrgs[i][1], -normalizedCorrs[i][0]])
+    return A
+  
+  def computeH(self):
+    
+    averageOrgs, averageCorrs = self.average()
+    
+    sOrgs = self.scale(self.orgs, averageOrgs)
+    sCorrs = self.scale(self.corrs, averageCorrs)
+    
+    T_normOrgs = self.matrixT(sOrgs, averageOrgs)
+    T_normCorrs = self.matrixT(sCorrs, averageCorrs)
+    
+    normalizedOrgs = self.normalize(self.orgs, T_normOrgs)
+    normalizedCorrs = self.normalize(self.corrs, T_normCorrs)
 
-    def __matrixT(points, average):
-        s = scale(points, average)
-        tX = s * (-average[0])
-        tY = s * (-average[1])
-        T = np.float64([[s, 0, tX], [0, s, tY], [0, 0, 1]])
-        return T
+    A = self.matrixA(normalizedOrgs, normalizedCorrs)
 
+    U, S, VT = np.linalg.svd(A)
+    V = VT.T
+    h = V[:, -1]
+    H_tilt = h.reshape(3, 3)
+    H = np.linalg.inv(T_norm_corrs) @ H_tilt @ T_norm_orgs
 
-    def __normalize(points, T):
-        normalizedPoints = np.zeros((len(points), 3))
-        i = 0
-        for point in points:
-            normalizedPoints[i] = np.transpose(T @ point)
-            i += 1
-        return normalizedPoints
-
-
-    def __matrixA(normalizedOrg, normalizedCorr):
-        A = np.zeros((len(normalizedOrg) * 2, 9))
-        i = 0
-        for index in range(0, len(A), 2):
-            A[index] = np.float64([0, 0, 0, -normalizedOrg[i][0], -normalizedOrg[i][1], -1, normalizedCorr[i]
-                                   [1] * normalizedOrg[i][0], normalizedCorr[i][1] * normalizedOrg[i][1], normalizedCorr[i][1]])
-            A[index + 1] = np.float64([normalizedOrg[i][0], normalizedOrg[i][1], 1, 0, 0, 0, -normalizedCorr[i]
-                                       [0] * normalizedOrg[i][0], -normalizedCorr[i][0] * normalizedOrg[i][1], -normalizedCorr[i][0]])
-            i += 1
-        return A
-
-
-    def computeH(vT, orgT, corrT):
-        averageOrg, averageCorr = average(src, dst)
-        orgT = matrixT(src, averageOrg)
-        corrT = matrixT(dst, averageCorr)
-        normalizedOrg = normalize(src, orgT)
-        normalizedCorr = normalize(dst, corrT)
-        A = matrixA(normalizedOrg, normalizedCorr)
-        U, S, vT = cv2.SVDecomp(A)
-
-        normalizedH = np.zeros((3, 3))
-        i = 0
-        for index in range(0, len(normalizedH)):
-            normalizedH[index][0] = vT[-1][i]
-            normalizedH[index][1] = vT[-1][i + 1]
-            normalizedH[index][2] = vT[-1][i + 2]
-            i += 3
-        H = inv(corrT) @ normalizedH @ orgT
-        return H
+    return H 
